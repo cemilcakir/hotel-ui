27407b32a4c4f2494b013e52c1bd679d4113e679
cemil <cemilcakir@outlook.com.tr>
2018-05-05 16:32:20 +0300

    admin paneli statik olarak eklendi

---
+++ src/AppBundle/Controller/AdminController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class AdminController extends Controller
+{
+    /**
+     * @Route("/admin",name="admin")
+     */
+    public function hotelIndexAction(){
+        return $this->render(':Otel:admin-index.html.twig');
+    }
+
+    /**
+     * @Route("/edit-hotel",name="edit-hotel")
+     */
+    public function editHotelAction(){
+        return $this->render(':Otel:admin-edit-hotel.html.twig');
+    }
+
+    /**
+     * @Route("/room-index",name="room-index")
+     */
+    public function roomIndexAction(){
+        return $this->render(':Otel:admin-room-index.html.twig');
+    }
+
+    /**
+     * @Route("/edit-room",name="edit-room")
+     */
+    public function editRoomAction(){
+        return $this->render(':Otel:admin-edit-room.html.twig');
+    }
+}
